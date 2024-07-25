052a00f553ed29cb2eea1c92128451db391108d1
Ashutosh <ashu7mishra@gmail.com>
2024-07-25 11:10:05 +0530

    react setup

---
+++ React1/example1/index.js
@@ -0,0 +1,12 @@
+class YooHoo extends React.Component{
+    render(){
+        return React.createElement(
+            'h2',
+            {},
+            'YooHoo React!'
+        );
+    }
+}
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(React.createElement(YooHoo), domContainer);
---
+++ React1/example2/index.js
@@ -0,0 +1,10 @@
+
+
+// const domContainer = document.querySelector('#root');
+// ReactDOM.render((<h2>Hello All!!!</h2>), domContainer);
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(
+  (<h1>YooHoo React!</h1>),
+  domContainer
+);
