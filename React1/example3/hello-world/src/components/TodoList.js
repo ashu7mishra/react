1d45d63164a14525fbdffc8aecf8077f3e0903b8
ashu7mishra <ashu7mishra@gmail.com>
2024-08-09 07:13:45 +0530

    todo list cont...

--- React1/example3/hello-world/src/components/TodoList.js
+++ React1/example3/hello-world/src/components/TodoList.js
@@ -1,14 +1,25 @@
 const TodoList = () => {
     const list = ['Item 1', 'Item 2','Item 3','Item 4','Item 5']
+
+    // const renderList = () => {
+    //     const data = [];
+    //     list.map(item => {
+    //         data.push(<li>{item}</li>)
+    //     })
+
+    //     return data
+    // }
+
     return (
         <div className="TodoWrapper">
             <h2>Todo List</h2>
             <ul>
                 {
-                    list.forEach(item => {
+                    list.map(item => {
                         return (<li>{item}</li>);
                     })
                 }
+                {/* {renderList()} */}
             </ul>
         </div>
     )
