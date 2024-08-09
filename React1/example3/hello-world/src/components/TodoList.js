66fc4ae03ac61f3c8b99394c1db6a4d0d500483f
ashu7mishra <ashu7mishra@gmail.com>
2024-08-09 10:02:42 +0530

    todo list cont...

--- React1/example3/hello-world/src/components/TodoList.js
+++ React1/example3/hello-world/src/components/TodoList.js
@@ -1,6 +1,7 @@
+import {useState} from "react"
 const TodoList = () => {
-    const list = ['Item 1', 'Item 2','Item 3','Item 4','Item 5']
-
+    const [list, setList] = useState(['Item 1', 'Item 2','Item 3','Item 4','Item 5']);
+    const[newItem, setNewItem] = useState('')
     // const renderList = () => {
     //     const data = [];
     //     list.map(item => {
@@ -10,9 +11,29 @@ const TodoList = () => {
     //     return data
     // }
 
+    const handleNewItemChange = (e) => {
+        // console.log(e.target.value);
+        setNewItem(e.target.value);
+    }
+
+    const handleButtonClick = () => {
+        // I want to know the value inside input text box
+        // Add the new item in list
+        // console.log(newItem);
+        // list.push(newItem);   //not working
+        setNewItem('')
+        const newList = [...list, newItem];
+        setList(newList);
+    }
+
     return (
         <div className="TodoWrapper">
             <h2>Todo List</h2>
+            <div>
+                <input type="text" placeholder='Add Item' value={newItem} onChange={handleNewItemChange}/>
+                <button onClick={handleButtonClick}>Add</button>
+            </div>
+            
             <ul>
                 {
                     list.map(item => {
