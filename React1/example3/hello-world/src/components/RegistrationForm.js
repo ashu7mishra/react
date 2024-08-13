60a33cbe2d2eaed57b4865a4a981a9940452d328
ashu7mishra <ashu7mishra@gmail.com>
2024-08-13 20:53:30 +0530

    registration form complete

--- React1/example3/hello-world/src/components/RegistrationForm.js
+++ React1/example3/hello-world/src/components/RegistrationForm.js
@@ -2,28 +2,57 @@
 // Password field
 // Phone number field
 // Batch selection field
+import { useState } from "react";
 
 const RegistrationForm = () =>{
-    const handleSubmit = () =>{
+    const [useName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [batch, setBatch] = useState('');
 
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log({
+            useName,
+            password,
+            phoneNumber,
+            batch
+        })
+    }
+    const usernameChange = (e) => {
+        console.log(e.target.value);
+        setUserName(e.target.value);
+    }
+    const passChange = (e) => {
+        console.log(e.target.value);
+        setPassword(e.target.value);
+    }
+    const phoneChange = (e) => {
+        console.log(e.target.value);
+        setPhoneNumber(e.target.value);
+    }
+    const batchChange = (e) => {
+        console.log(e.target.value);
+        setBatch(e.target.value);
     }
     return (
         <form onSubmit={handleSubmit}>
             <div>
                 <label>Username </label>
-                <input type="text" placeholder="Enter Username"/>
+                <input type="text" placeholder="Enter Username" onChange={usernameChange}/>
             </div>
             <div>
                 <label>Password </label>
-                <input type="password" placeholder="Enter Password"/>
+                <input type="password" placeholder="Enter Password" onChange={passChange}/>
             </div>
             <div>
                 <label>Phone </label>
-                <input type="number" placeholder="Enter Contact Number"/>
+                <input type="number" placeholder="Enter Contact Number" onChange={phoneChange}/>
             </div>
             <div>
                 <label>Batch </label>
-                <select>
+                <select onChange={batchChange}>
                     <option>Frontend</option>
                     <option>Backend</option>
                     <option>Fullstack</option>
