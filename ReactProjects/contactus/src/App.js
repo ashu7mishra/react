f244eddb6fd22dd2b234188dea2e29e5a11c8ae8
Ashutosh <ashu7mishra@gmail.com>
2024-08-19 15:29:29 +0530

    contact us

---
+++ ReactProjects/contactus/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react';
+import Navigation from './components/Navigation';
+
+function App() {
+  // const [count, setCount] = useState(0)
+  return (
+    <div>
+      <Navigation/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactProjects/contactus/src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <nav>
+        <div className='logo'>
+            <img src='/images/logo.png' alt='do some coding logo'/>
+        </div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navigation
